5ea4a13831349852047f55da2612e7399f990929
Kurumi30 <bsd3su@gmail.com>
2022-12-01 23:39:19 -0300

    corrigir o CSS e implementar o download

---
+++ app.js
@@ -0,0 +1,93 @@
+let type = "sfw"
+let category = "waifu"
+const categories = [
+    "waifu",
+    "neko",
+    "shinobu",
+    "megumin",
+    "bully",
+    "cuddle",
+    "cry",
+    "kiss",
+    "lick",
+    "hug",
+    "awoo",
+    "pat",
+    "smug",
+    "bonk",
+    "yeet",
+    "blush",
+    "smile",
+    "wave",
+    "highfive",
+    "handhold",
+    "nom",
+    "bite",
+    "glomp",
+    "slap",
+    "kill",
+    "kick",
+    "happy",
+    "wink",
+    "poke",
+    "dance",
+    "cringe",
+]
+let api
+let imageSection = document.getElementById('imageSection')
+let generateWaifuBtn = document.getElementById('generateWaifu')
+let loading = document.getElementById('loading')
+let downloadLink = document.getElementById('downloadLink')
+let select = document.getElementById('select')
+let options = ""
+
+for (c of categories) {
+    options += `<option value="${c}">${c}</option>`
+}
+
+select.innerHTML = `
+<select id="selectInput" class="form-select form-select-sm" aria-label=".form-select-sm example">
+<option value="waifu" selected>Escolha uma categoria</option>
+${options}
+</select>
+`
+
+generateWaifuBtn.addEventListener("click", () => {
+    let selected = document.getElementById('selectInput')
+
+    category = selected.value
+    api = `https://api.waifu.pics/${type}/${category}`
+    generateWaifuBtn.innerHTML = `
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Carregando...</span>
+    </div>
+`
+imageSection.innerHTML = ""
+imageSection.innerHTML = imageSkeleton
+
+generateWaifu()
+})
+
+const imageSkeleton = `
+<div class="card">
+    <div class="card-body">
+        <div class="card-title placeholder-glow">
+            <span class="col-12 placeholder" id="span"></span>
+        </div>
+    </div>
+</div>
+`
+
+function generateWaifu() {
+    fetch(api).then(response => {
+        return response.json()
+    }).then(data => {
+        imageSection.innerHTML = `
+        <img src="${data.url}" class="img-thumbnail d-block my-3" id="waifuImage" alt="waifu image">
+        `
+        downloadLink.href = data.url
+        generateWaifuBtn.innerHTML = '<b>Gerar mais...</b>'
+    }).catch(error => {
+        generateWaifuBtn.innerHTML = 'Erro ao gerar...'
+    })
+}
